65f955da0c7f871e0ef6cb6b3d6486f0059c7c66
nelson <azees1995@gmail.com>
2024-12-07 21:43:24 +0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+/**
+ * Utility function to combine and merge class names
+ * using clsx and tailwind-merge for conditional and optimized class management.
+ */
+export function cn(...inputs: ClassValue[]): string {
+  // Merge the classnames using clsx and tailwind-merge
+  return twMerge(clsx(...inputs));
 }
