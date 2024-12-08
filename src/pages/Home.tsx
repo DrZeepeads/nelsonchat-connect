99bd1fde0c338b888e235fb30af09b4856523726
nelson <175583803+DrZeepeads@users.noreply.github.com>
2024-12-09 01:13:22 +0500

    improved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,25 @@ import { AppSidebar } from "../components/AppSidebar";
 import ChatArea from "../components/ChatArea";
 
 const Home: React.FC = () => (
-  <div className="min-h-screen flex flex-col">
+  <div className="home min-h-screen flex flex-col">
+    {/* Header Section */}
     <Header />
-    <div className="flex-grow flex">
-      <aside className="w-64">
+
+    {/* Main Content */}
+    <div className="home__content flex-grow flex">
+      {/* Sidebar */}
+      <aside
+        className="home__sidebar w-64 md:w-48 lg:w-64 bg-gray-100 dark:bg-gray-900"
+        aria-label="Application Sidebar"
+      >
         <AppSidebar />
       </aside>
-      <main className="flex-grow">
+
+      {/* Chat Area */}
+      <main
+        className="home__chat flex-grow bg-white dark:bg-gray-800"
+        aria-label="Chat Area"
+      >
         <ChatArea />
       </main>
     </div>
